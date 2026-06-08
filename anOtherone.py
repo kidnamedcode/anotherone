f2e22e57f9eb0e62da1e2ea9fe0395f625bfc1c3
kidnamedcode <aaron.gajurel12@gmail.com>
2026-06-08 21:32:32 +1000

    We are communicating we are learning

--- anOtherone.py
+++ anOtherone.py
@@ -1,9 +1,29 @@
 import tkinter as tk
 window = tk.Tk()
 
-window.title("We the best music")
+window.title("We the best")
 window.geometry("800x800")
 
+## Where the function goes:
+## 'def submit_answer'
 
 
-window.mainloop()
+question_label = tk.Label(
+    window,
+    text = "Hackclub please approve my stuff im new to this"
+)
+question_label.pack()
+question_label.pack(pady=100)
+
+answer_entry = tk.Entry(window)
+answer = answer_entry.get()
+
+command = submit_answer
+
+submit_button = tk.Button(
+    window, 
+    text="Submit",
+    command= submit_answer
+)
+
+window.mainloop()       
