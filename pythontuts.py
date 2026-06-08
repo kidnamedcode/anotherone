e68065d0a37bac69f77f1669c9dbcbe2bf40f574
kidnamedcode <aaron.gajurel12@gmail.com>
2026-06-08 22:18:25 +1000

    python function tuts

---
+++ pythontuts.py
@@ -0,0 +1,24 @@
+def f(x):
+    return x + 2
+y = f(3) + f(10)
+print(y)
+
+
+
+print("Okay your gonna have to type in your most recent exam mark.")
+print("this system relies on HONESTY")
+def function(something):
+    return something + " ...great! or not?"
+idk = function(input("Enter the mark..."))
+print(idk)
+
+
+print("Ok new function exercise")
+print("Lets complete a story of sorts")
+
+def functionNew(DAMN):
+    return "So I was taking a walk the other day..." + DAMN
+print("Okay the story starter is: 'So I was taking a walk the other day...")
+print("continue.")
+idk2 = functionNew(input("Enter:"))
+print(idk2)
